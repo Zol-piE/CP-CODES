c39eed9aada669d8ad10e209e1d2b911b9f7f566
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2025-09-27 13:25:53 +0600

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+#define rep(a,b) for(int i =a;i<b;i++)
+#define OP ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+signed main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        
+    }
+    return 0;
+}
