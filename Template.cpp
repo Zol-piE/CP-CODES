2a522bfb65cb00177249204033c15840a20f85c9
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2026-07-16 16:23:29 +0600

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -1,15 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define endl '\n'
-#define yes cout << "YES\n";
-#define no cout << "NO\n";
-using ll = long long;
-const ll MOD = 1e9 + 7;
-const ll INF = LLONG_MAX >> 1;
-#define rep(a, b) for (ll i = (a); i < (b); i++)
-#define ZolpiE                        \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(nullptr)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define int long long
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+#define ZolpiE ios_base::sync_with_stdio(false), cin.tie(nullptr)
 using vi = vector<int>;
 struct custom_hash
 {
@@ -29,7 +26,7 @@ struct custom_hash
 };
 void pr(vi &arr, int n)
 {
-    for (ll i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
     cout << endl;
 }
